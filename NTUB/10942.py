0bc052ef007e03463f48e68f76346a08117dd169
rtashklzx47277 <rtashklzx47277@gmail.com>
2023-03-03 18:41:32 +0800

    20230303

--- NTUB/10942.py
+++ NTUB/10942.py
@@ -56,6 +56,13 @@ data = '''8
 1 2
 2 1'''
 
+def dfs(visited, node):
+  visited[node - 1] = True
+  count = 1
+  for neighbor in graph[node]:
+    if not visited[neighbor - 1]: count += dfs(visited, neighbor)
+  return count
+
 lines = data.splitlines()
 times = int(lines[0])
 end = 1
@@ -63,50 +70,21 @@ for i in range(times):
   start = end
   nums = int(lines[start])
   end = start + nums + 1
-  graph = [[] for _ in range(nums)]
 
-  for j in range(nums):
-    u, v = map(int, lines[start + 1:end][j].split())
-    graph[u - 1].append(v - 1)
-    graph[v - 1].append(u - 1)
+  graph = {}
+  for i in range(nums):
+    graph.update({i + 1: []})
 
-  # edges = []
-  # for line in lines[start + 1:end]:
-  #   edges.append([int(num) for num in line.split()])
-  # edges.sort(key = lambda node: node[0])
-
-  # dict = {}
-  # for i in range(nums): dict[i + 1] = set()
-
-  # check = None
-  # for i in range(nums):
-  #   temp = edges[i][1]
-  #   while temp != i + 1 and temp not in dict[i + 1]:
-  #     dict[i + 1].add(temp)
-  #     temp = edges[temp - 1][1]
-  #   if len(dict[i + 1]) == nums - 1:
-  #     check = i + 1
-  #     break
-
-  # if check: print(check)
-  # else: print(max(dict, key = lambda node: len(dict[node])))
-
-T = int(input())
-for i in range(T):
-    N = int(input())
-    graph = [[] for _ in range(N)]
-    for j in range(N):
-        u, v = map(int, input().split())
-        graph[u-1].append(v-1)
-        graph[v-1].append(u-1)
-
-    # 找到連通分量中最小度數的節點
-    min_degree = sys.maxsize
-    start_node = 0
-    for j in range(N):
-        if len(graph[j]) < min_degree:
-            min_degree = len(graph[j])
-            start_node = j
-    
-    # 將特殊郵件發送給 start_node，因為它有最小度數
-    print(f"Case {i+1}: {start_node+1}")
+  for line in lines[start + 1:end]:
+    u, v = [int(num) for num in line.split()]
+    graph[u].append(v)
+  
+  start, count = 1, 0
+  for node in graph:
+    visit = [None for _ in range(nums)]
+    temp = dfs(visit, node)
+    if count < temp:
+      count = temp
+      start = node
+    elif count == temp and start > node: start = node 
+  print(start)
